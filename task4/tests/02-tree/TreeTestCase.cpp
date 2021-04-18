58b18348e612fd945f3670304a1690dc4e45a732
Raman Svistunou <roma-svistunov@tut.by>
2021-04-18 17:25:24 +0300

    Update TreeTestCase.cpp
    TreeTestCase?

--- task4/tests/02-tree/TreeTestCase.cpp
+++ task4/tests/02-tree/TreeTestCase.cpp
@@ -5,4 +5,29 @@
 
 #include "TreeTestCase.h"
 #include "Tree.h"
-
+#include "TreeTestCase.h"
+#include "Tree.h"
+TEST(A, B) {
+    boost::filesystem::create_directory("./TreeTestCaseTmpDir");
+    boost::filesystem::create_directory("./TreeTestCaseTmpDir/inner");
+    std::ofstream file("./TreeTestCaseTmpDir/file.txt");
+    EXPECT_THROW(GetTree("./TreeTestCaseTmpDirNO", false), std::invalid_argument);
+    EXPECT_THROW(GetTree("./TreeTestCaseTmpDir/file.txt", false), std::invalid_argument);
+    auto node1 = GetTree("./TreeTestCaseTmpDir", true);
+    auto node2 = GetTree("./TreeTestCaseTmpDir", false);
+    EXPECT_EQ(node1.name, "TreeTestCaseTmpDir");
+    EXPECT_EQ(node2.name, "TreeTestCaseTmpDir");
+    EXPECT_TRUE(node1.is_dir);
+    EXPECT_TRUE(node2.is_dir);
+    for (auto& sn : node1.children) {
+        if (sn.is_dir) {
+            EXPECT_EQ(sn.name, "inner");
+            EXPECT_EQ(sn.children.size(), 0);
+        } else {
+            EXPECT_EQ(sn.name, "file.txt");
+        }
+    }
+    FilterEmptyNodes(node2, ".");
+    EXPECT_FALSE(boost::filesystem::exists("./TreeTestCaseTmpDir/inner"));
+    boost::filesystem::remove_all("TreeTestCaseTmpDir");
+}
