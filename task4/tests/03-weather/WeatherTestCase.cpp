0350f5408741982b9338fb22eb74e1222086784c
Raman Svistunou <roma-svistunov@tut.by>
2021-04-18 21:06:59 +0300

    maybe

--- task4/tests/03-weather/WeatherTestCase.cpp
+++ task4/tests/03-weather/WeatherTestCase.cpp
@@ -8,12 +8,12 @@
 TEST(A, B) {
     WeatherMock mock;
     mock.SetApiKey("key");
-    ASSERT_EQ(mock.GetTemperature("1 2"), 1);
-    ASSERT_EQ(mock.GetDifferenceString("1 2", "2 3"), "Weather in 1 2 is colder than in 2 3 by 1 degrees");
-    ASSERT_EQ(mock.GetDifferenceString("2 2", "1 3"), "Weather in 2 2 is warmer than in 1 3 by 1 degrees");
-    ASSERT_EQ(mock.GetTomorrowDiff("10 100"), "The weather in 10 100 tomorrow will be much warmer than today.");
-    ASSERT_EQ(mock.GetTomorrowDiff("10 11"), "The weather in 10 11 tomorrow will be warmer than today.");
-    ASSERT_EQ(mock.GetTomorrowDiff("10 10"), "The weather in 10 10 tomorrow will be the same than today.");
-    ASSERT_EQ(mock.GetTomorrowDiff("10 9"), "The weather in 10 9 tomorrow will be colder than today.");
-    ASSERT_EQ(mock.GetTomorrowDiff("10 5"), "The weather in 10 5 tomorrow will be much colder than today.");
+    EXPECT_EQ(mock.GetTemperature("1 2"), 1);
+    EXPECT_EQ(mock.GetDifferenceString("1 2", "2 3"), "Weather in 1 2 is colder than in 2 3 by 1 degrees");
+    EXPECT_EQ(mock.GetDifferenceString("2 2", "1 3"), "Weather in 2 2 is warmer than in 1 3 by 1 degrees");
+    EXPECT_EQ(mock.GetTomorrowDiff("10 100"), "The weather in 10 100 tomorrow will be much warmer than today.");
+    EXPECT_EQ(mock.GetTomorrowDiff("10 11"), "The weather in 10 11 tomorrow will be warmer than today.");
+    EXPECT_EQ(mock.GetTomorrowDiff("10 10"), "The weather in 10 10 tomorrow will be the same than today.");
+    EXPECT_EQ(mock.GetTomorrowDiff("10 9"), "The weather in 10 9 tomorrow will be colder than today.");
+    EXPECT_EQ(mock.GetTomorrowDiff("10 5"), "The weather in 10 5 tomorrow will be much colder than today.");
 }
