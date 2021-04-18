a14d1b93b643f3c4f73c56d2e817f7b1c023edd4
Raman Svistunou <roma-svistunov@tut.by>
2021-04-18 21:19:54 +0300

    dbg

--- task4/tests/03-weather/WeatherTestCase.cpp
+++ task4/tests/03-weather/WeatherTestCase.cpp
@@ -11,6 +11,7 @@ TEST(Weather, Mocked) {
     EXPECT_EQ(mock.GetTemperature("1 2"), 1);
     EXPECT_EQ(mock.GetDifferenceString("1 2", "2 3"), "Weather in 1 2 is colder than in 2 3 by 1 degrees");
     EXPECT_EQ(mock.GetDifferenceString("2 2", "1 3"), "Weather in 2 2 is warmer than in 1 3 by 1 degrees");
+    std::cout << mock.GetTomorrowDiff("10 100");
     EXPECT_EQ(mock.GetTomorrowDiff("10 100"), "The weather in 10 100 tomorrow will be much warmer than today.");
     EXPECT_EQ(mock.GetTomorrowDiff("10 11"), "The weather in 10 11 tomorrow will be warmer than today.");
     EXPECT_EQ(mock.GetTomorrowDiff("10 10"), "The weather in 10 10 tomorrow will be the same than today.");
